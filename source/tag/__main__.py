b4c638b22f9c83d8e09ea73972444b706be6b6dd
Nathan Dunfield <nathan@dunfield.info>
2018-02-15 10:00:23 -0600

    Python 3 compatibility fix: raw_input -> input

--- source/tag/__main__.py
+++ source/tag/__main__.py
@@ -1,5 +1,6 @@
 
 import os
+from six.moves import input
 
 from PyPDF2 import PdfFileReader
 try:
@@ -21,43 +22,43 @@ if __name__ == '__main__':
 	parser.add_argument('--rooms', type=str, help='path to room spreadsheet')
 	parser.add_argument('--config', type=str, default='', help='use config file')
 	args = parser.parse_args()
-	
+
 	exams = []
 	for exam in args.exams:
 		with open(exam, 'rb') as exam_input:
 			exams.append(PdfFileReader(StringIO(exam_input.read())))
-	
+
 	page_count = exams[0].getNumPages()
 	for exam in exams:
 		if exam.getNumPages() != page_count:
 			raise ValueError('Not all exams have the same number of pages.')
-	
+
 	if args.config:
 		scores, request, spares, start = parse_file(args.config)
-		
+
 		print('Using scoring system: %s' % scores)
 		print('Assigning:')
 		for room, room_name, num_exams in request:
 			print('\t%d seats in %s' % (num_exams, room))
-		
+
 		import openpyxl  # Slow import so move it later.
 		W = openpyxl.load_workbook(args.rooms)
-		
+
 		cumulative = start if args.start is None else args.start
 		for room, room_name, num_exams in request:
 			exam_room = Room.from_sheet(room_name, W.get_sheet_by_name(room))
-			
+
 			exam_seats = exam_room.subroom(num_exams).exam_seats(start=cumulative) + \
 				[ExamSeat('Spare %d (%s)' % (i+1, room_name), cumulative + num_exams + i, i) for i in range(spares)]
-			
+
 			cumulative += len(exam_seats)
-			
+
 			main(args.exams, scores, exam_seats, os.path.join(args.output, '%s.pdf' % room_name))
 	else:
 		if args.start is None: args.start = 1
-		
+
 		if args.num is None:
-			raw_num = raw_input('Number of exams needed: ')
+			raw_num = input('Number of exams needed: ')
 			if not raw_num: exit()
 		else:
 			raw_num = args.num
@@ -65,9 +66,9 @@ if __name__ == '__main__':
 			args.num = int(raw_num)
 		except ValueError:
 			raise ValueError('Couldn\'t parse "%s" as a number of exams' % raw_num)
-		
+
 		if args.scores is None:
-			raw_scores = raw_input('Enter points available per page as a comma separated list (or blank to cancel).\nExpecting %d page scores.\nUnless you want to award points for completing the cover sheet, this should start with a 0: ' % page_count)
+			raw_scores = input('Enter points available per page as a comma separated list (or blank to cancel).\nExpecting %d page scores.\nUnless you want to award points for completing the cover sheet, this should start with a 0: ' % page_count)
 			if not raw_scores: exit()
 		else:
 			raw_scores = args.scores
@@ -75,14 +76,13 @@ if __name__ == '__main__':
 			scores = [int(i) for i in raw_scores.split(',')]
 		except ValueError:
 			raise ValueError('Couldn\'t parse "%s" as comma separated list of scores' % raw_scores)
-		
+
 		if len(scores) != page_count:
 			raise ValueError('Expected %d page scores but got %d page scores.' % (page_count, len(scores)))
 		if not all(0 <= score <= 10 for score in scores):
 			raise ValueError('Scores must be in the range [0, 10].')
-		
+
 		print('Using scoring system: %s' % scores)
 		exam_seats = [ExamSeat('', exam_num, exam_num) for exam_num in range(args.start, args.start + args.num)]
-		
-		main(args.exams, scores, exam_seats, os.path.join(args.output, 'output.pdf'))
 
+		main(args.exams, scores, exam_seats, os.path.join(args.output, 'output.pdf'))

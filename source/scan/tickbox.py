0aacabe26d5b3b74a2f917ba592cfcb43d931c19
Nathan Dunfield <nathan@dunfield.info>
2018-04-18 20:50:27 -0500

    Dealing with sloppy large ticks, bug fixes to manual.py

--- source/scan/tickbox.py
+++ source/scan/tickbox.py
@@ -19,26 +19,26 @@ def tick_box_locations(image):
 	'''
 	h, w = image.shape
 	ys = [int(0.25*h), int(0.75*h)]
-	
+
 	start = w / 41.4285
 	boxwidth = w / 18.125
 	boxspacing = w / 11.1538
 	xs = [(int(x), int(x+boxwidth)) for x in np.arange(start, w, boxspacing)]
-	
+
 	return xs, ys
 
 def raw_read_tickboxes(score_area, show=True):
 	''' Return a list of (black density of center box, black density of full box, tickbox number). '''
 	xs, (y0, y1) = tick_box_locations(score_area)
 	boxes = [score_area[y0:y1, x0:x1] for x0, x1 in xs]
-	
+
 	def center_box(x0, x1):
 		y2 = int(0.75*y0 + 0.25*y1)
 		y3 = int(0.25*y0 + 0.75*y1)
 		x2 = int(0.75*x0 + 0.25*x1)
 		x3 = int(0.25*x0 + 0.75*x1)
 		return score_area[y2:y3, x2:x3]
-	
+
 	small_boxes = [center_box(x0, x1) for x0, x1 in xs]
 	return [(image_utils.black_density(small_boxes[i]), image_utils.black_density(boxes[i]), i) for i in range(len(boxes))]
 
@@ -47,21 +47,21 @@ def winner(densities, column):
 	return data[0][-1], data[0][column], data[0][column] / data[1][column]
 
 def most_solid_big(densities):
-	return max([(d[1] / d[0], d[-1]) for d in densities if d[0] > 0.25])
+	return max([(d[1] / d[0], d[-1]) for d in densities if d[0] > 0.25 or d[1] > 0.17])
 
 def read_tickbox(image):
 	tb = extract_tickbox(image)
 	densities = raw_read_tickboxes(tb)
-	
+
 	score_small, density_small, ratio_small = winner(densities, 0)
 	score_big, density_big, ratio_big = winner(densities, 1)
 	max_ratio = max(ratio_small, ratio_big)
-	
-	if density_small < 0.25:
+
+	if density_small < 0.25 and density_big < 0.17:
 		raise ScoreReadError('Boxes nearly blank')
-	
+
 	most_solid, score_solid = most_solid_big(densities)
-	
+
 	if most_solid > 0.7 and score_solid == score_big and ratio_big > 1.2:
 		return score_big, ratio_big
 	if score_small == score_big and max_ratio > 1.2:
@@ -73,4 +73,3 @@ def read_tickbox(image):
 	if ratio_small > 1.1 and density_small > 0.5:
 		return score_small, ratio_small
 	raise ScoreReadError('Ambiguous')
-

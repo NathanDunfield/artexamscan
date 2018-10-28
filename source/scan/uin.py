af1f75e1595af3882b7b69813b39c99b9acfcf4a
Nathan Dunfield <nathan@dunfield.info>
2018-10-27 22:14:39 -0500

    Skipping scans with wrong page size; dealing with sloppy canceling of unused exams

--- source/scan/uin.py
+++ source/scan/uin.py
@@ -11,20 +11,25 @@ NUM_DIGITS = 9
 CHARS = '0123456789'
 
 def extract_uin_box(image):
-    # Slice off the bottom of the page and rotate to make horizontal
     return image_utils.extract_box(image[720:2400, 1700:3300], align='vertical')
 
 def read_uin(page_image):
     # Get the UIN box.
     image = extract_uin_box(page_image)
-
     height, width = image.shape
-    dy = height / 10.75  # Should change.
+    dy = height / 10.75
     dx = width / (NUM_DIGITS + 1)
+    if dy < 0.95 * dx:  # Test for carelessly striking through unused exams
+        return 9*'0'
+    if dy > 1.1 * dx:  # Likely detected top of UIN box by mistake
+        dy = dx
+        height = int(10.75 * dy)
+        image = image[-height:]
     vertlines = [int(p) for p in np.arange(0.5 * dx, width, dx)]
-    horlines = [int(p) for p in np.arange(0.25 * dy, height, dy)]  # Should change.
+    horlines = [int(p) for p in np.arange(0.25 * dy, height, dy)]
 
-    densities = [sorted([(image_utils.black_density(box), char) for char, box in zip(CHARS, np.vsplit(column, horlines)[1:-1])], reverse=True) for column in np.hsplit(image, vertlines)[1:-1]]
+    densities = [sorted([(image_utils.black_density(box), char) for char, box in zip(CHARS, np.vsplit(column, horlines)[1:-1])], reverse=True)
+                 for column in np.hsplit(image, vertlines)[1:-1]]
 
     uin = [column[0][1] for column in densities]
     lightest = min(column[0][0] for column in densities)
